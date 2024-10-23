01ee4d8143dad9b023ebf2f5885ef29f210b0674
adams <adamskiptalam0@gmail.com>
2024-10-23 13:08:07 +0300

    Create changeColor() to generate random colors and change square bg-colors.

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div')
         square.classList.add("square");
-        square.textContent = "Square" + i;
+        //square.textContent = "Square" + i;
         gridContainer.appendChild(square);
-        console.log(square);
+        //console.log(square);
+        square.addEventListener('mouseover', () => changeColor(square));
     }
+}
+
+function changeColor(square) {
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    square.style.backgroundColor = randomColor;
 }
 
 createGrid(16);
