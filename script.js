92c1fd5e31337bb850f4f158a92e4fe7e369c757
adams <adamskiptalam0@gmail.com>
2024-10-23 13:53:06 +0300

    Add event listener to get grid size from prompt.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const gridContainer = document.querySelector('.container');
+const sizeBtn = document.querySelector('.sizeBtn');
+const resetBtn = document.querySelector('.resetBtn');
+
+
 
 
 function createGrid(size) {
+    // getGridSize();
+    gridContainer.innerHTML = '';
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div')
         square.classList.add("square");
@@ -16,5 +22,16 @@ function changeColor(square) {
     const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
     square.style.backgroundColor = randomColor;
 }
+
+sizeBtn.addEventListener('click', () => {
+        let size = parseInt(prompt("Enter grid size: "));
+        if (!size) {
+            alert("Enter a valid input between 1 - 100.");
+        } else if (size > 100) {
+            size = 100;
+        }
+        createGrid(size);
+    }
+    );
 
 createGrid(16);
