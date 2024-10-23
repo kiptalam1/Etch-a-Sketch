4e7d5ba863b8c1a97f5792931134b9eb4bd73c2f
adams <adamskiptalam0@gmail.com>
2024-10-23 12:35:17 +0300

    Change createGrid function to accomodate size*size squares.

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const gridContainer = document.querySelector('.container');
 
 
 function createGrid(size) {
-    for (let i = 1; i <= size; i++) {
-        const newDivs = document.createElement('div')
-        newDivs.classList.add("div" + i);
-        gridContainer.appendChild(newDivs);
-        console.log(newDivs);
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div')
+        square.classList.add("square");
+        square.textContent = "Square" + i;
+        gridContainer.appendChild(square);
+        console.log(square);
     }
 }
 
